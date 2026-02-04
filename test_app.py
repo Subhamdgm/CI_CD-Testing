5f721344d9fc7cb9714ae40fde6d79efb9b4ad8b
Subhamdgm <subhamdgms@gmail.com>
2026-02-04 17:37:01 +0530

    Make some more test cases

--- test_app.py
+++ test_app.py
@@ -4,3 +4,9 @@ def test_add():
     assert add(2,3)==5
 def test_output():
     assert add(3,4)==8
+def test_out():
+    assert add(1,1)==2
+def test_out1():
+    assert add(2,2)==5
+def test_out2():
+    assert add(2,3)==4
